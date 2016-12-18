22dfb7b6d23c40ac4a0d85ec4da7149db1dc067a
Brandon Lawrence <bmlawrence@protonmail.ch>
2016-12-18 00:16:58 -0600

    style changes, color clock addition

--- js/custom.js
+++ js/custom.js
@@ -4,21 +4,32 @@ function doClock() {
 
   var month = d.getMonth();
   var date  = d.getDate();
+  var day   = d.getDay();
   var year  = d.getFullYear();
   var hours = d.getHours();
   var mins  = d.getMinutes();
   var secs  = d.getSeconds();
   var ampm  = hours < 12 ? "AM" : "PM";
 
-  hours = hours%12   ? hours%12 : 12;
+  // hours = hours%12   ? hours%12 : 12;  // 12 hour modifier
   hours = hours <= 9 ? "0" + hours : hours;
   mins  = mins <= 9  ? "0" + mins  : mins;
   secs  = secs <= 9  ? "0" + secs  : secs;
 
-  $('#clock').html(hours+":"+mins+":"+secs+" "+ampm);
-  $('#date').html((month+1)+"."+date+"."+year);
+  $('.time').html(hours+":"+mins+":"+secs+" "+ampm);
 
-  var timeout = setTimeout(doClock, 500);
+  var months =
+    ["January", "February", "March", "April", "May", "June", "July",
+     "August", "September", "October", "November", "December"];
+  var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  $('.date').html(days[day]+", "+months[month]+" "+date+", "+year);
+
+  var hex = "#" + hours + mins + secs;
+  $("body").css("background-color", hex);
+  $('.hex').html(hex);
+
+  setTimeout(doClock, 500);
 }
 
 // Onload
